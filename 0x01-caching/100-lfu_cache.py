d131b048e1ee01418c2790191f47e8d6ec35b675
Nawaritta <Nor.benhaddou@gmail.com>
2024-02-01 02:27:10 +0000

    adv task check

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -9,21 +9,41 @@ class LFUCache(BaseCaching):
     def __init__(self):
         """constructor"""
         super().__init__()
+        self.freq = {}
 
     def put(self, key, item):
         """assign item to key"""
         if key and item:
-            if key in self.cache_data:
-                self.cache_data.pop(key)
-            self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                lfu_key = list(self.cache_data)[-2]
+            for k in self.cache_data:
+                if k not in self.freq:
+                    self.freq[k] = 1
+                else:
+                    self.freq[k] += 1
+
+            mlfu = 0
+            lfu_key = None
+            for k in self.freq:
+                if self.freq[k] > mlfu:
+                    mlfu = self.freq[k]
+                    lfu_key = k
+
+            if lfu_key and lfu_key in self.cache_data:
                 self.cache_data.pop(lfu_key)
                 print("DISCARD: {}".format(lfu_key))
-        pass
+
+            if key in self.cache_data:
+                self.freq[key] += 1
+            else:
+                self.freq[key] = 1
+
+            self.cache_data[key] = item
 
     def get(self, key):
         """gets the key value"""
         if key and key in self.cache_data:
+            value = self.cache_data.pop(key)
+            self.freq[key] += 1
+            self.put(key, value)
+
             return self.cache_data[key]
         return None
